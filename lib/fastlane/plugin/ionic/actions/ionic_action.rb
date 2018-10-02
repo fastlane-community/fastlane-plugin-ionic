218c1f9650ebcf4a2cb317abc530ade30f1b9468
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2018-10-02 12:31:43 +0200

    debug

--- lib/fastlane/plugin/ionic/actions/ionic_action.rb
+++ lib/fastlane/plugin/ionic/actions/ionic_action.rb
@@ -30,17 +30,10 @@ module Fastlane
         args_map.each do |action_key, cli_param|
           param_value = params[action_key]
 
-          puts cli_param
-          puts param_value
-          puts action_key
-          puts param_value.kind_of?(Array)
-          puts action_key.to_s == 'build_flag'
-
           if action_key.to_s == 'build_flag' && param_value.kind_of?(Array)
-            puts "Build flag is an array!"
             unless param_value.empty?
               param_value.each do |flag|
-                platform_args << "--#{cli_param}=#{Shellwords.escape(flag)}"
+                platform_args << "--#{cli_param}=#{flag}"
               end
             end
           else
