a8aacbe2344e1e6ff8a267d4493c40dfa939419c
Jan Piotrowski <piotrowski@gmail.com>
2018-12-10 23:15:18 +0100

    extract methods an move plarform_args stuff (and joining) around

--- lib/fastlane/plugin/ionic/actions/ionic_action.rb
+++ lib/fastlane/plugin/ionic/actions/ionic_action.rb
@@ -47,7 +47,7 @@ module Fastlane
           end
         end
 
-        return platform_args.join(' ')
+        return platform_args
       end
 
       def self.get_android_args(params)
@@ -94,8 +94,7 @@ module Fastlane
         return config.elements['widget'].elements['name'].first.value # TODO: Simplify!? (Check logic in cordova)
       end
 
-      # actual building! (run step #2)
-      def self.build(params)
+      def self.get_args(params)
         args = [params[:release] ? '--release' : '--debug']
         args << '--device' if params[:device]
         args << '--prod' if params[:prod]
@@ -105,14 +104,17 @@ module Fastlane
           args << "--buildConfig=#{params[:cordova_build_config_file].shellescape}"
         end
 
-        android_args = self.get_android_args(params) if params[:platform].to_s == 'android'
-        ios_args = self.get_ios_args(params) if params[:platform].to_s == 'ios'
+        return args
+      end
 
+      def self.prepare(params, args)
         if params[:cordova_prepare]
           # TODO: Remove params not allowed/used for `prepare`
           sh "ionic cordova prepare #{params[:platform]} --no-interactive #{args.join(' ')}"
         end
+      end
 
+      def self.update_build_number(params)
         # special handling for `build_number` param
         if params[:platform].to_s == 'ios' && !params[:build_number].to_s.empty?
           cf_bundle_version = params[:build_number].to_s
@@ -124,12 +126,24 @@ module Fastlane
             }
           )
         end
+      end
 
+      def self.compile(params, args)
         if params[:platform].to_s == 'ios'
-          sh "ionic cordova compile #{params[:platform]} --no-interactive #{args.join(' ')} -- #{ios_args}" 
+          android_args = self.get_android_args(params)
+          sh "ionic cordova compile #{params[:platform]} --no-interactive #{args.join(' ')} -- #{ios_args.join(' ')}" 
         elsif params[:platform].to_s == 'android'
-          sh "ionic cordova compile #{params[:platform]} --no-interactive #{args.join(' ')} -- -- #{android_args}" 
+          ios_args = self.get_ios_args(params)
+          sh "ionic cordova compile #{params[:platform]} --no-interactive #{args.join(' ')} -- -- #{android_args.join(' ')}" 
         end
+      end
+
+      # actual building! (run step #2)
+      def self.build(params)
+        args = self.get_args(params)
+        self.prepare(params, args)
+        self.update_build_number(params)
+        self.compile(params, args)
       end
 
       # export build paths (run step #3)
