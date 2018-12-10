10a5a63bc709c6d8563eddab4677bf5dac047e94
Jan Piotrowski <piotrowski@gmail.com>
2018-12-10 23:15:53 +0100

    test get_args

--- spec/ionic_action_spec.rb
+++ spec/ionic_action_spec.rb
@@ -40,5 +40,33 @@ describe Fastlane::Actions::IonicAction do
       end
     end
   end
+
+  describe '#get_args' do
+    let (:params) { {} }
+
+        #params[:release] ? '--release' : '--debug']
+        #args << '--device' if params[:device]
+        #args << '--prod' if params[:prod]
+        #args << '--browserify' if params[:browserify]
+        #if !params[:cordova_build_config_file].to_s.empty?
+        #  args << "--buildConfig=#{
+
+    it 'none' do
+      result = Fastlane::Actions::IonicAction.get_args(params);
+      expect(result).to eq(['--debug'])
+    end
+
+    it 'release, device, prod, browserify, cordova_build_config_file' do
+      params = { :release => true, :device => true, :prod => true, :browserify => true, :cordova_build_config_file => 'foo'}
+      result = Fastlane::Actions::IonicAction.get_args(params);
+      expect(result).to eql(['--release', '--device', '--prod', '--browserify', '--buildConfig=foo'])
+    end
+
+    it 'cordova_build_config_file = path with space' do
+      path = "path with space"
+      params = { :cordova_build_config_file => path}
+      result = Fastlane::Actions::IonicAction.get_args(params);
+      expect(result).to eql(['--debug', "--buildConfig=#{path.shellescape}"])
+    end
   end
 end
